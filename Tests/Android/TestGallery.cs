db54f3eae2e69708239afe9560f69e04e64d0206
romovs <romovs@gmail.com>
2022-01-04 15:26:26 +0200

    Add more methods and attributes

--- Tests/Android/TestGallery.cs
+++ Tests/Android/TestGallery.cs
@@ -15,5 +15,28 @@ namespace nunit_mobile.Tests.Android
             var logo = driver.FindElementByAccessibilityId("More options");
             logo.Click();
         }
+
+        [Test(Description = "Open the menu + MULTIPLE TEST CASES"), Category("Menu")]
+        [TestCase("param1", Description = "OpenMoreOptions2 with param1"), Category("Menu")]
+        [TestCase("param2", Description = "OpenMoreOptions2 with param2"), Category("Menu")]
+        public void OpenMoreOptions2()
+        {
+            var logo = driver.FindElementByAccessibilityId("More options");
+            logo.Click();
+        }
+
+        [Test(Description = "Ordered tests 1"), Category("Ordered"), Order(1)]
+        public void OpenMoreOptions3()
+        {
+            var logo = driver.FindElementByAccessibilityId("More options");
+            logo.Click();
+        }
+
+        [Test(Description = "Ordered tests 2"), Category("Ordered"), Order(2)]
+        public void OpenMoreOptions4()
+        {
+            var logo = driver.FindElementByAccessibilityId("More options");
+            logo.Click();
+        }
     }
 }
